cda641b74e5bcb6978c445db7dec738396c94246
Gordon Franke <info@nevalon.de>
2013-10-17 14:16:37 +0200

    add template parameter to jmeter_generator.php file use die instate of exception

--- tests/jmeter_generator.php
+++ tests/jmeter_generator.php
@@ -7,28 +7,37 @@
  * file that was distributed with this source code.
  */
 
-if (!isset($argv[1])) {
-    throw new Exception('You must provide a log path argument.');
-}
-$logPath = $argv[1];
-if (!is_dir($logPath)) {
-    throw new Exception(sprintf('Log path %s argument is not a directory.', $logPath));
-}
-
-if (!isset($argv[2])) {
-    throw new Exception('You must provide a output file path argument.');
-}
-$outputPath = $argv[2];
-if (file_exists($outputPath)) {
-    if (!is_writeable($outputPath)) {
-        throw new Exception(sprintf('Output file path %s argument is not writeable.', $outputPath));
-    }
+ if (!isset($argv[1])) {
+    die('You must provide a log path argument.' . PHP_EOL);
+ }
+ $logPath = $argv[1];
+ if (!is_dir($logPath)) {
+    die(sprintf('Log path %s argument is not a directory.', $logPath) . PHP_EOL);
+ }
+ 
+ if (!isset($argv[2])) {
+    die('You must provide a output file path argument.' . PHP_EOL);
+ }
+ $outputPath = $argv[2];
+ if (file_exists($outputPath)) {
+     if (!is_writeable($outputPath)) {
+        die(sprintf('Output file path %s argument is not writeable.', $outputPath) . PHP_EOL);
+     }
+ } else {
+     touch($outputPath);
+ }
+ 
+if (!isset($argv[3])) {
+    $template = __DIR__ . '/access_log.jmx';
 } else {
-    touch($outputPath);
+    $template = $argv[3];
+    if (!is_readable($template)) {
+        die(sprintf('Template file %s is not readable.', $template) . PHP_EOL);
+    }
 }
 
 $doc = new DOMDocument();
-$doc->load(__DIR__ . '/access_log.jmx');
+$doc->load($template);
 
 $xpath = new domXPath($doc);
 
